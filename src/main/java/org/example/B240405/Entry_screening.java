dac89bfd137b6ee5b36cf184f3635b3edbaf8033
Bae Ji Won <qowhxk@naver.com>
2024-11-15 12:14:29 +0900

    입국심사

---
+++ src/main/java/org/example/B240405/Entry_screening.java
@@ -0,0 +1,28 @@
+package org.example.B240405;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+/*
+    백준 3079번 문제 : 입국심사
+
+    https://www.acmicpc.net/problem/3079
+ */
+public class Entry_screening {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        for(int i=0; i<N; i++){
+
+        }
+    }
+}
